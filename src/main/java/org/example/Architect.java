2cfd17a3ac113df10d31eafe28c22caa2e8dae11
KaveyaAnnadurai <kaveya.a@m2pfintech.com>
2023-03-30 11:54:34 +0530

    Requirement1: Add. Included testcase and logic to check if length is same or not.

--- src/main/java/org/example/Architect.java
+++ src/main/java/org/example/Architect.java
@@ -2,4 +2,18 @@ package org.example;
 
 public class Architect {
 
+    private int length;
+
+    private String units;
+
+   public Architect(int length, String units){
+        this.length = length;
+        this.units = units;
+    }
+
+    public Boolean checkLength(int lengthFromUser, String unitsFromUser){
+        if(length == lengthFromUser && units.equals(unitsFromUser))
+            return true;
+        else return false;
+    }
 }
--- src/test/java/com/m2p/ArchitectTest.java
+++ src/test/java/com/m2p/ArchitectTest.java
@@ -1,7 +1,24 @@
 package com.m2p;
 
 import org.example.Architect;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 
 public class ArchitectTest {
-    Architect architect = new Architect();
+    @Test
+    void checkIfOneCmIsOneCm(){
+        //arrange
+        Architect architect = new Architect(1,"cm");
+        Boolean expectedAnswer = true;
+
+        //act
+        Boolean actualAnswer = architect.checkLength(1,"cm");
+
+        //assert
+        assertThat(expectedAnswer, is(equalTo(actualAnswer)));
+    }
 }
